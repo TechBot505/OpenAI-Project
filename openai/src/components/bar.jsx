c679a1e31361bd4f527a03f0a2e03f2b378efbf3
Akanksha <akankshaprasad184@gmail.com>
2023-08-28 00:57:55 +0530

    front2

--- openai/src/components/bar.jsx
+++ openai/src/components/bar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import log from "../assets/Name.png"
 
 export default function Bar() {
   return (
     <div class="navbar">
-     <div className="logo">SUMMARIZR</div>
+     <div className="logo"><img src={log}/></div>
 
   <div className="brand">IIT Indore<div className="small">Indian Institute Of Technology Indore</div></div>
 
